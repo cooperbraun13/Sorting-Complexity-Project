7da3a1d1ce90968c0333b8a2ead4c132ea3f4e7d
ADH-17 <ayden.humphries17@gmail.com>
2025-03-21 10:11:43 -0700

    Started the creation of evaluator.hpp and test_evaluator.cpp

---
+++ evaluator.hpp
@@ -0,0 +1,19 @@
+#ifndef EVALUATOR_HPP
+#define EVALUATOR_HPP
+#include <vector>
+
+class Evaluator {
+public:
+std::vector <std::vector<int>> eval_vec;
+
+void ingest();
+
+private:
+
+};
+
+//ingest() // delimits on whitespace take in a number delimit, for four lines then update. 
+// fill vector with numbers in list, same with doubly linked lists
+// variables vector of doubly linked lists and vector and one for the time that it takes from 
+//chrono library convert into something more readable
+#endif
---
+++ test_evaluator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void test_evaluator_merge_comparison(){
+
+}
+
+void test_evaluator_quick_comparison(){
+
+}
+
+int main(){
+
+}
